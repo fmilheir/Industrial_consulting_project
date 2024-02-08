8cdc56b3c6fe0450532d0998f9f60b4473c4eb4b
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2024-02-08 12:41:43 +0000

    Working containers

---
+++ frontend/src/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>  
+        <h1>Hello World</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message: ''
+        };
+    },
+    mounted() {
+        // make a GET request to the Flash backend
+        fetch('/message')
+        .then(response => response.json())
+        .then(data => {
+            this.message = data.message;
+        })
+        .catch(error => {
+            console.error('Error fetching message:', error);
+        }); 
+    }
+};
