c55130228f7bc1785060af1a19283606bc78d3b3
Boldizsarb <donboyszy@gmail.com>
2024-02-09 17:37:15 +0000

    database connection established

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, jsonify 
-
+from database import test_db_connection
 app = Flask(__name__)
+
+
+
+
+
+
+## POOL IS IN THE DATABASE FILE
+
 
 @app.route('/message', methods=['GET'])
 def get_message():
     return jsonify({'message': 'Hello, World!'})
 
+@app.route('/database', methods=['GET']) ## checking if database is connecting
+def get_database():
+    return test_db_connection()
+
+
+
 if __name__ == '__main__':
+    print('hello workd this is a print statement ')
+
     app.run(host= '0.0.0.0', port=5000, debug=True)
-print('hello workd')
+
---
+++ backend/database.py
@@ -0,0 +1,65 @@
+import psycopg2
+from psycopg2 import pool
+
+class DBPool:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if DBPool._instance is None:
+            DBPool._instance = pool.ThreadedConnectionPool(minconn=1, maxconn=10,
+                                                           user="postgres",
+                                                           password="postgres",
+                                                           host="postgresql",
+                                                           port="5432",
+                                                           database='industrial_consulting')
+        return DBPool._instance
+
+def create_table_user_if_not_exists():
+    conn = None
+    try:
+        conn = DBPool.get_instance().getconn()
+        cur = conn.cursor()
+
+        cur.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'user')")
+        table_exists = cur.fetchone()[0]
+
+        if not table_exists:
+            cur.execute("""
+                CREATE TABLE "user" (
+                    id SERIAL PRIMARY KEY,
+                    first_name VARCHAR(255) NOT NULL,
+                    last_name VARCHAR(255) NOT NULL,
+                    email VARCHAR(255) NOT NULL UNIQUE,
+                    number VARCHAR(20)
+                )
+            """)
+            conn.commit()
+            return "Table 'user' created successfully."
+        else:
+            return "Table 'user' already exists."
+
+    except psycopg2.Error as e:
+        return f"Unable to create table: {e}"
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            DBPool.get_instance().putconn(conn)
+
+def test_db_connection():
+    conn = None
+    try:
+        conn = DBPool.get_instance().getconn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM test LIMIT 1")
+
+        row = cur.fetchone()
+        return f"Database connection successful. Test query result: {row} User table: {create_table_user_if_not_exists()}"
+    except psycopg2.Error as e:
+        return f"Unable to connect to the database: {e}"
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            DBPool.get_instance().putconn(conn)
